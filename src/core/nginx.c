e4737a269db024881118a5576636d7bf70d6b9a0
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-09 18:14:11 +0900

    Add libnginx_init

--- src/core/nginx.c
+++ src/core/nginx.c
@@ -198,7 +198,40 @@ static char        *ngx_signal;
 static char **ngx_os_environ;
 
 
-#ifndef LIBNGINX
+#ifdef LIBNGINX
+
+int libnginx_init(ngx_log_t **log, const char *log_filename, unsigned log_level, ngx_uint_t use_stderr)
+{
+    ngx_debug_init();
+    int ret = ngx_strerror_init();
+    if (ret != NGX_OK) {
+        return ret;
+    }
+    ngx_time_init();
+
+    ngx_pid = ngx_getpid();
+    ngx_parent = ngx_getppid();
+
+    ngx_log_t *l = *log = ngx_log_init_name((u_char *) log_filename);
+    l->log_level = log_level;
+    ngx_use_stderr = use_stderr;
+
+    ngx_cycle_t init_cycle;
+    ngx_memzero(&init_cycle, sizeof(ngx_cycle_t));
+    init_cycle.log = l;
+    ngx_cycle = &init_cycle;
+
+    ret = ngx_os_init(l);
+    if (ret != NGX_OK) {
+        return ret;
+    }
+    ngx_slab_sizes_init();
+
+    return NGX_OK;
+}
+
+#else
+
 int ngx_cdecl
 main(int argc, char *const *argv)
 {
--- src/core/ngx_core.h
+++ src/core/ngx_core.h
@@ -110,4 +110,8 @@ void ngx_cpuinfo(void);
 #define NGX_DISABLE_SYMLINKS_NOTOWNER   2
 #endif
 
+#ifdef LIBNGINX
+int libnginx_init(ngx_log_t **log, const char *log_filename, unsigned log_level, ngx_uint_t use_stderr);
+#endif
+
 #endif /* _NGX_CORE_H_INCLUDED_ */
