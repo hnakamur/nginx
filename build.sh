0834e91881bfe0b976f29ba6f361ced2aeb58020
Hiroaki Nakamura <hnakamur@gmail.com>
2023-12-27 13:22:51 +0900

    Create /tmp/random-1g.dat

--- build.sh
+++ build.sh
@@ -87,6 +87,9 @@ systemctl status nginx
 if [ ! -f /tmp/random-1m.dat ]; then
   dd if=/dev/random of=/tmp/random-1m.dat bs=1048576 count=1
 fi
+if [ ! -f /tmp/random-1g.dat ]; then
+  dd if=/dev/random of=/tmp/random-1g.dat bs=1048576 count=1024
+fi
 hey -host www1.example.com -c 3 -z 3s -m POST -D /tmp/random-1m.dat -req-body-rate 262144 -req-body-burst 262144 http://localhost/limit-conn &
 hey -host www1.example.com -c 3 -z 3s http://localhost/limit-conn &
 hey -host www2.example.com -c 9 -z 3s http://localhost/limit-conn &
