db355770f486867107416933a967d288b6d90d1e
Hiroaki Nakamura <hnakamur@gmail.com>
2023-12-29 23:48:31 +0900

    Adjust sleep duration of watch_nginx_memory.sh

--- watch_nginx_memory.sh
+++ watch_nginx_memory.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-while :; do echo $(date +%Y-%m-%dT%H:%M:%S) $(systemctl show --property=MemoryCurrent nginx); sleep 0.5; done
+while :; do echo $(date +%Y-%m-%dT%H:%M:%S) $(systemctl show --property=MemoryCurrent nginx); sleep 0.99; done
